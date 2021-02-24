c7ed88696c40a2e1c2dd3ff278307afec156ea80
Maksim Trojan <maksim.trojan@dfinsolutions.com>
2021-02-24 14:36:05 +0200

    it works

--- tests/createNewClient.spec.js
+++ tests/createNewClient.spec.js
@@ -1,17 +1,27 @@
 import { expect } from 'chai';
+import { createClient } from '../sourceBooksApis/clientApi';
 import 'chai-http';
-import {createClient} from '../sourceBooksApis/clientApi';
 import * as chai from 'chai';
 chai.use(require('chai-http'));
-let clientName;
+export let clientName;
 
-describe('Client create test sute', async () => {
-    it('should test that possible to create new client', async () => {
-        const timeStamp = new Date().toLocaleString();
-        clientName = `Name of organization ${timeStamp}`;
-        const responseFromClienCreate = await createClient(clientName);
+describe('Client create test suite', async () => {
+  it('Should test that it is possible to create new client', async () => {
+    const timestamp = new Date().toLocaleString();
+    clientName = `Name of organization${timestamp}`;
+    const responseFromClientCreate = await createClient(clientName);
 
-        expect(responseFromClienCreate).to.have.status(200);
-        expect(responseFromClienCreate.data).to.haveOwnProperty('clientId').to.be.not.null;
-    });
+    expect(responseFromClientCreate).to.have.status(200);
+    expect(responseFromClientCreate.data).to.haveOwnProperty('clientId').to.be
+      .not.null;
+  });
+  it('Should test that it is not possible to create new client', async () => {
+    const notValidClientName = '';
+    const responseFromClientCreate = await createClient(notValidClientName);
+
+    expect(responseFromClientCreate).to.have.status(400);
+    expect(responseFromClientCreate.data)
+      .to.haveOwnProperty('message')
+      .equals('Invalid body, check \'errors\' property for more info.');
+  });
 });
